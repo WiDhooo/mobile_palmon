32d7179c9f2400444d1cae7e2dab2bb6d3a13f21
fzn-ops <ojanjan423@gmail.com>
2024-11-04 11:22:49 +0700

    coba

--- app/src/main/java/com/example/project_palm_on/home.java
+++ app/src/main/java/com/example/project_palm_on/home.java
@@ -3,6 +3,7 @@ package com.example.project_palm_on;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
@@ -12,14 +13,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.project_palm_on.databinding.ActivityHomeBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class home extends AppCompatActivity {
-    ImageButton icon_kalkulasi;
+    ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new home_fragment());
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -27,40 +35,25 @@ public class home extends AppCompatActivity {
             return insets;
         });
 
-        //def objek
-        icon_kalkulasi = findViewById(R.id.icon_kalkulasi_home);
-
-        //arah ke kalkulasi
-        icon_kalkulasi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(home.this, kalkulasi_page.class);
-                startActivity(i);
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_home) {
+                replaceFragment(new home_fragment());
+            } else if (item.getItemId() == R.id.nav_toko) {
+                replaceFragment(new toko());
+            } else if (item.getItemId() == R.id.nav_history) {
+                replaceFragment(new history());
+            } else if (item.getItemId() == R.id.nav_akun) {
+                replaceFragment(new account());
             }
+            return true;
         });
 
-        //ini gw nyoba doang bisa aktif gk buttonnya
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                if (id == R.id.nav_home) {
-                    Toast.makeText(home.this, "Home Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-                } else if (id == R.id.nav_toko) {
-                    Toast.makeText(home.this, "Toko Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-                } else if (id == R.id.nav_history) {
-                    Toast.makeText(home.this, "History Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-                } else if (id == R.id.nav_akun) {
-                    Toast.makeText(home.this, "Akun Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                return false;
-            }
-
-        });
+    }
+    public void replaceFragment(Fragment fragment){
+        Log.d("Fragment", "Replacing with: " + fragment.getClass().getSimpleName());
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
