f50c2e19a61bf1ebb9dfaa3b6fd4306c15e3e2ed
Ridho Putra Pratama <ridhoputra714@gmail.com>
2024-11-07 04:45:10 +0700

    Tampilan confirm dan back

--- app/src/main/java/com/example/project_palm_on/catat_kalkulasi.java
+++ app/src/main/java/com/example/project_palm_on/catat_kalkulasi.java
@@ -1,7 +1,9 @@
 package com.example.project_palm_on;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,9 +18,9 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -29,7 +31,6 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class catat_kalkulasi extends AppCompatActivity implements View.OnClickListener {
 
     EditText tglPanen, hargaTbs, beratTotalTbs, potonganTimbangan, upahPanen, biayaTransportasi, biayaLainnya;
@@ -39,8 +40,9 @@ public class catat_kalkulasi extends AppCompatActivity implements View.OnClickLi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_catat_kalkulasi);
+
+        // Optional: Apply edge-to-edge insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -61,7 +63,68 @@ public class catat_kalkulasi extends AppCompatActivity implements View.OnClickLi
 
         buttonHitung.setOnClickListener(this);
         buttonKembali.setOnClickListener(this);
-        };
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button_hitung_catat_kalkulasi) {
+            final Dialog dialog = new Dialog(catat_kalkulasi.this);
+            dialog.setContentView(R.layout.dv_hitungkalkulasi_confirm);
+            if (dialog.getWindow() != null) {
+                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent); // Set background transparan
+            }
+            dialog.show();
+
+            Button confirm = dialog.findViewById(R.id.btn_ya_kalkulasi);
+            confirm.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.green_login)));
+            Button back = dialog.findViewById(R.id.btn_kembali_kalkulasi);
+
+            confirm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    hitungHasil();
+                    dialog.dismiss();
+                    Intent i = new Intent(catat_kalkulasi.this, kalkulasi_page.class);
+                    startActivity(i);
+                }
+            });
+
+            back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+
+        } else if (view.getId() == R.id.button_kembali_catat_kalkulasi) {
+            final Dialog dialog = new Dialog(catat_kalkulasi.this);
+            dialog.setContentView(R.layout.dv_hitungkalkulasi_cancel);
+            if (dialog.getWindow() != null) {
+                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent); // Set background transparan
+            }
+            dialog.show();
+
+            Button confirm = dialog.findViewById(R.id.btn_lanjutkan_kalkulasi);
+            confirm.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.green_login)));
+            Button back = dialog.findViewById(R.id.btn_batal_kalkulasi);
+
+            confirm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+
+            back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                    Intent i = new Intent(catat_kalkulasi.this, kalkulasi_page.class);
+                    startActivity(i);
+                }
+            });
+        }
+    }
 
     private void hitungHasil() {
         String tglpanen = tglPanen.getText().toString().trim();
@@ -74,19 +137,17 @@ public class catat_kalkulasi extends AppCompatActivity implements View.OnClickLi
 
         progressDialog.setMessage("Menghitung Data...");
         progressDialog.show();
+
         StringRequest stringRequest = new StringRequest(Request.Method.POST,
                 Constants.URL_KALKULASI,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         progressDialog.dismiss();
-
                         try {
                             JSONObject jsonObject = new JSONObject(response);
-
                             Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
-
-                        } catch (JSONException e){
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
@@ -97,7 +158,7 @@ public class catat_kalkulasi extends AppCompatActivity implements View.OnClickLi
                         progressDialog.hide();
                         Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
                     }
-                }){
+                }) {
             @Nullable
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
@@ -110,23 +171,10 @@ public class catat_kalkulasi extends AppCompatActivity implements View.OnClickLi
                 params.put("biaya_transportasi", biayatransportasi);
                 params.put("biaya_lainnya", biayalainnya);
                 return params;
-
-
             }
         };
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
-    }
-
-    public void onClick(View view) {
-        if (view.getId() == R.id.button_hitung_catat_kalkulasi) {
-            hitungHasil();
-            Intent i = new Intent(catat_kalkulasi.this, kalkulasi_page.class);
-            startActivity(i);
-        } else if (view.getId() == R.id.button_kembali_catat_kalkulasi) { // Ganti dengan ID yang benar
-            Intent i = new Intent(catat_kalkulasi.this, kalkulasi_page.class);
-            startActivity(i);
-        }
     }
 }
