72d7cb1788ba4a40621d3410411fcb39ff03e79a
Ridho Putra Pratama <ridhoputra714@gmail.com>
2024-11-10 20:15:31 +0700

    test push

--- app/src/main/java/com/example/project_palm_on/Constants.java
+++ app/src/main/java/com/example/project_palm_on/Constants.java
@@ -3,6 +3,6 @@ package com.example.project_palm_on;
 public class Constants {
     private static final String ROOT_URL = "http://10.0.2.2:8000/api/";
     public static final String URL_KALKULASI = ROOT_URL + "kalkulasi";
-
+    public static final String URL_ARTIKEL = ROOT_URL + "artikel";
 
 }
